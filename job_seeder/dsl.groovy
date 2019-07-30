b8821335d4e23a0f976ddcdefd7a69bf61d6fe10
Tarun Arora 4 <tarora8@sapient.com>
2019-07-30 15:12:03 +0530

    Adding seeder files

---
+++ job_seeder/dsl.groovy
@@ -0,0 +1,28 @@
+def createPipelineJob(def jobName, def repoName) {
+    return pipelineJob(jobName) {
+        def repository = "https://github.com/sujan-reads/my-maven-project.git"
+
+        description 'CI/CD Pipeline'
+
+        triggers {
+            scm('* * * * *')
+        }
+
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(repository)
+                        }
+                        branches('master')
+                        scriptPath('Jenkinsfile')
+                        extensions {} //Required to prevent Git tag
+                    }
+                }
+            }
+        }
+    }
+}
+
+createPipelineJob('My-Maven-Project','my-maven-project')
