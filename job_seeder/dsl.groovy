c44ca5ec6d5754819119584a25b222a4091311c8
tarunarora20 <51780119+tarunarora20@users.noreply.github.com>
2019-07-30 17:36:11 +0530

    Update dsl.groovy

--- job_seeder/dsl.groovy
+++ job_seeder/dsl.groovy
@@ -1,15 +1,11 @@
 def gitUrl = 'https://github.com/tarunarora20/my-maven-project.git'
 
-job('build-artifact') {
+mavenjob('build-artifact') {
     scm {
         git(gitUrl)
     }
     triggers {
         scm('*/1 * * * *')
     }
-    steps {
-        withEnv(["PATH+MVN=${tool 'Maven'}/bin"]) {
-            sh "mvn -e clean install"
-            }
-        }
+    goals('-e clean install')
     }
