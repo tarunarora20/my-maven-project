a2797c0fc43e8b8a43fead5751bbf59221bd84e8
tarunarora20 <51780119+tarunarora20@users.noreply.github.com>
2019-07-30 16:28:11 +0530

    Update dsl.groovy

--- job_seeder/dsl.groovy
+++ job_seeder/dsl.groovy
@@ -3,7 +3,6 @@ def gitUrl = 'https://github.com/tarunarora20/my-maven-project.git'
 job('build-artifact') {
     scm {
         git(gitUrl)
-        extensions {}
     }
     triggers {
         scm('*/1 * * * *')
